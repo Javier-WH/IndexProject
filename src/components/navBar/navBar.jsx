8224556c6804a4d4478ac3454f1f50b867eadefb
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2022-10-16 13:52:38 -0400

    print

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -15,7 +15,7 @@ import ReactToPrint from "react-to-print";
 
 export function NavBar() {
 
-    const { SaveData, studentList } = useContext(MainContext)
+    const { SaveData, studentList, activeSeccion } = useContext(MainContext)
 
     const componentRef = useRef();
 
@@ -24,6 +24,24 @@ export function NavBar() {
         color: "white"
     }
 
+    function translateGrades(student) {
+        try {
+            let grades = student.subjects[activeSeccion.split(" ")[0]];
+            let l1 = grades.lap1;
+            let l2 = grades.lap2;
+            let l3 = grades.lap3;
+            let def = grades.def;
+            return <>
+                <td>{l1}</td>
+                <td>{l2}</td>
+                <td>{l3}</td>
+                <td>{def}</td>
+            </>
+        } catch (error) {
+
+        }
+
+    }
     let key = 1;
     return <>
 
@@ -44,55 +62,45 @@ export function NavBar() {
                     <SubjectMenu sx={iconsStyle} />
                 </div>
             </div>
-            <div id="print" ref={componentRef}>
-                <div id="printTitleContainer">
-                    <img src={Logo} alt="" id="printLogo" />
-                    <div>
-                        <div className="printTitle">U.E. Colegio Batalla de la Victoria</div>
-                        <div className="printTitle">Asignatura: Física</div>
-                        <div className="printTitle">Sección: 3B</div>
-                        <div className="printTitle">Profesor: Antonio Magallanes</div>
-                        <div className="printTitle">Periodo escolar 2022 - 2023</div>
-                    </div>
+        </div>
+        <div id="print" ref={componentRef}>
+            <div id="printTitleContainer">
+                <img src={Logo} alt="" id="printLogo" />
+                <div>
+                    <div className="printTitle">U.E. Colegio Batalla de la Victoria</div>
+                    <div className="printTitle">Asignatura: Física</div>
+                    <div className="printTitle">Sección: 3B</div>
+                    <div className="printTitle">Profesor: Antonio Magallanes</div>
+                    <div className="printTitle">Periodo escolar 2022 - 2023</div>
                 </div>
-
-                <table id="printTable">
-                    <thead id="printThead">
-                        <tr>
-                            <th scope="col">N°</th>
-                            <th scope="col">Cédula</th>
-                            <th scope="col">Nombres y Apellidos</th>
-                            <th scope="col">1er Lapso</th>
-                            <th scope="col">2do Lapso</th>
-                            <th scope="col">3er Lapso</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        {
-                            studentList.map(student => {
-                                return <tr key={key++}>
-                                    <td>{key}</td>
-                                    <td>{student.ci}</td>
-                                    <td>{`${student.names} ${student.lastNames}`}</td>
-                                    <td>@mdo</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                            })
-                        }
-
-
-                    </tbody>
-                </table>
-
-                {
-                    studentList.map(student => {
-                        return <div>Hola</div>
-                    })
-                }
-
             </div>
+
+            <table id="printTable">
+                <thead id="printThead">
+                    <tr>
+                        <th scope="col">N°</th>
+                        <th scope="col">Cédula</th>
+                        <th scope="col">Nombres y Apellidos</th>
+                        <th scope="col">1er Lapso</th>
+                        <th scope="col">2do Lapso</th>
+                        <th scope="col">3er Lapso</th>
+                        <th scope="col">def</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        studentList.map(student => {
+                            return <tr key={key++}>
+                                <td>{key}</td>
+                                <td>{student.ci}</td>
+                                <td>{`${student.names} ${student.lastNames}`}</td>
+                                {translateGrades(student)}
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+
         </div>
     </>
 }
