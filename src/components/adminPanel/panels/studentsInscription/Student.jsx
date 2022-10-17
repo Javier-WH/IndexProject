e54dcd07dd89a4771a7815a48a4057f86569aac7
Francisco Javier Rodriguez Hernandez <fj_rh@hotmail.com>
2022-10-17 14:38:48 -0400

    panel estudiantes

--- src/components/adminPanel/panels/studentsInscription/Student.jsx
+++ src/components/adminPanel/panels/studentsInscription/Student.jsx
@@ -1,8 +1,13 @@
 import FormControlLabel from '@mui/material/FormControlLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import Switch from '@mui/material/Switch';
-import { useState } from 'react';
+
+
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 import "./student.css"
 
@@ -18,9 +23,30 @@ export default function Inscription() {
         <div id="studentDataContainer">
             <TextField id="outlined-basic" label="Nombres" variant="outlined" />
             <TextField id="outlined-basic" label="Apellidos" variant="outlined" />
-            <div id="auxContainer">
-                <TextField id="outlined-basic" label="Cumpleaños" variant="outlined" type="date" />
+
+
+            <TextField id="outlined-basic" label="Cumpleaños" variant="outlined" type="date" />
+            <TextField id="outlined-basic" label="Télefono" variant="outlined" type="number" />
+            <TextField id="outlined-basic" label="Email" variant="outlined" type="email" />
+
+            <FormControl>
+                <FormLabel id="demo-radio-buttons-group-label">Género</FormLabel>
+                <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="female"
+                    name="radio-buttons-group"
+                >
+                    <FormControlLabel value="f" control={<Radio />} label="Femenino" />
+                    <FormControlLabel value="m" control={<Radio />} label="Masculino" />
+                </RadioGroup>
+            </FormControl>
+
+            <div id="requisites">
+                <FormControlLabel control={<Checkbox defaultChecked />} label="Label" />
+
             </div>
+
+
         </div>
 
     </div>
